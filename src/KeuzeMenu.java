e5c974b2afb9bac4552d88bd7a6bcc067aabbcc0
dylandoldersum <dylandoldersum@outlook.com>
2020-04-21 16:10:25 +0200

    Update KeuzeMenu.java

--- src/KeuzeMenu.java
+++ src/KeuzeMenu.java
@@ -3,6 +3,10 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * @author dylan
+ */
+
 public class KeuzeMenu extends JFrame {
 
     public KeuzeMenu() {
