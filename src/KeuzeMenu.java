3d0546f14d62972473f38b64dcd2a4a4f312b9d3
dylandoldersum <dylandoldersum@outlook.com>
2020-04-21 14:42:42 +0200

    DbReader

--- src/KeuzeMenu.java
+++ src/KeuzeMenu.java
@@ -1,9 +1,28 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class KeuzeMenu extends JFrame {
 
     public KeuzeMenu() {
+
+        /** JFrame **/
         super("Routebepaling - Keuzemenu");
+        setSize(600, 400);
+
+        /** Buttons **/
+        JButton btn1 = new JButton("Huidige routes");
+        JButton btn2 = new JButton("Route inplannen");
+        JButton btn3 = new JButton("Route starten");
+        JButton btn4 = new JButton("Gemaakte ritten");
+        JButton btn5 = new JButton("Beheren route");
+        JButton btn6 = new JButton("Beheren adressen");
+        JButton btn7 = new JButton("Beheren bezorgers");
+
+        for(int i = 0; i < 7; i++) {
+            add(btn1);
+        }
+
+        setLayout(new FlowLayout());
         setVisible(true);
     }
 
