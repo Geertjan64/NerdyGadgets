f533dcdbd6fa6b0787ed040ebea7db42b732e619
PAhrary <pamirahrary@gmail.com>
2020-04-21 14:20:04 +0200

    test of hij de out ignored

--- src/Bezorgers.java
+++ src/Bezorgers.java
@@ -4,18 +4,24 @@ import java.awt.*;
 public class Bezorgers extends JFrame {
 
     private JLabel titel;
+    private JLabel naam;
+    private JLabel aantalBezorgingen;
     private JList bezorgerlijst;
 
     public Bezorgers() {
         titel = new JLabel("Beheren bezorgers");
+        naam = new JLabel("Naam");
+        aantalBezorgingen = new JLabel("Aantal bezorgingen");
         bezorgerlijst = new JList();
 
         setTitle("Bezorgers beheren");
         setSize(1000, 600);
-        setLayout(new GridLayout(4, 3));
+        setLayout(new GridLayout(1, 3));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         add(titel);
+        add(naam);
+        add(aantalBezorgingen);
 
         setVisible(true);
 
