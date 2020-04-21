0e6e8221b9bc9ac56266e6845b7cdc9e3dc8ab67
dylandoldersum <dylandoldersum@outlook.com>
2020-04-21 15:02:15 +0200

    JDBC added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
+import java.sql.SQLException;
+
 public class Main {
-
-    public static void main(String[] args) {
-
-
-
+    public static void main(String[] args) throws SQLException {
+        DatabaseReader dbr = new DatabaseReader();
+        dbr.getDataRows();
     }
 }
