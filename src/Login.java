f9dba709f1a754cdd27aca696a701763ad0279fd
PascalBoertien <57621478+PascalBoertien@users.noreply.github.com>
2020-04-23 11:46:32 +0200

    login check added

--- src/Login.java
+++ src/Login.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 
-public class Login extends JFrame{
+public class Login extends JFrame implements ActionListener {
     JButton blogin;
     JPanel loginpanel;
     JTextField txuser;
@@ -21,26 +20,42 @@ public class Login extends JFrame{
         username = new JLabel("User - ");
         password = new JLabel("Pass - ");
 
-        setSize(1000,800);
-        setLocation(500,280);
-        loginpanel.setLayout (null);
+        setSize(1000, 800);
+        setLocation(500, 280);
+        loginpanel.setLayout(null);
 
 
-        txuser.setBounds(70,30,150,20);
-        pass.setBounds(70,65,150,20);
-        blogin.setBounds(110,100,80,20);
-        username.setBounds(20,28,80,20);
-        password.setBounds(20,63,80,20);
+        txuser.setBounds(70, 30, 150, 20);
+        pass.setBounds(70, 65, 150, 20);
+        blogin.setBounds(110, 100, 80, 20);
+        username.setBounds(20, 28, 80, 20);
+        password.setBounds(20, 63, 80, 20);
 
         loginpanel.add(blogin);
         loginpanel.add(txuser);
         loginpanel.add(pass);
         loginpanel.add(username);
         loginpanel.add(password);
+        blogin.addActionListener(this);
 
         getContentPane().add(loginpanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        //login button
+        if (e.getSource() == blogin) {
+            String userText;
+            String pwdText;
+            userText = txuser.getText();
+            pwdText = pass.getText();
+            if (userText.equalsIgnoreCase("testmed") && pwdText.equalsIgnoreCase("test123")) {
+                JOptionPane.showMessageDialog(this, "Login Successful");
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid Username or Password");
+            }
+        }
     }
 
     public static void main(String[] args) {
