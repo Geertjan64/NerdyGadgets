563686528a34b31e5dc3e8fdb211df2af307ba47
Lars <s1147485@student.windesheim.nl>
2020-04-24 10:56:25 +0200

    EditCustomer

--- src/EditCustomer.java
+++ src/EditCustomer.java
@@ -1,2 +1,76 @@
-public class EditCustomer {
-}
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+class MySQLTable
+{
+    // Define database variables
+    private static Connection con = null;
+    private static String URL = "jdbc:mysql://localhost:3306/wideworldimporters";
+    private static String driver = "com.mysql.jdbc.Driver";
+    private static String user = "root";
+    private static String pass = "";
+    private Object JTable;
+
+    /**
+     * Main aplication entry point
+     * @param args
+     * @throws SQLException
+     */
+    public static void main(String[] args) throws SQLException
+    {
+
+        Statement stmt;
+        String query;
+        ResultSet rs;
+
+        // Temporary data
+        Object rowData[][] = {{"Row1-Column1", "Row1-Column2", "Row1-Column3", "Row1-Column4"}};
+        // Array for columnNames
+        Object columnNames[] = {"Klant naam", " Adres", "Postcode", "Stad", ""};
+
+        // Creating table
+        DefaultTableModel mTableModel = new DefaultTableModel(rowData, columnNames);
+        JTable table = new JTable(mTableModel);
+
+        // try & connect DB
+        try {
+            Class.forName(driver).newInstance();
+            con = DriverManager.getConnection(URL, user, pass);
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+
+        // Query from customers and cities
+        query = "SELECT CustomerName, DeliveryAddressLine2, DeliveryPostalCode, CityName FROM Customers join cities on DeliveryCityID = CityID";
+        stmt = con.createStatement();
+        // Execute query and return results
+        rs = stmt.executeQuery(query);
+
+        // Creating JFrame
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JScrollPane scrollPane = new JScrollPane(table);
+        frame.add(scrollPane, BorderLayout.CENTER);
+        frame.setSize(600, 500);
+        frame.setVisible(true);
+
+        // remove the temp row previously created
+        mTableModel.removeRow(0);
+
+        Object[] rows;
+        // For each row
+        while (rs.next()) {
+            // adding values to temporary rows
+            rows = new Object[]{rs.getString(1), rs.getString(2), rs.getString(3),rs.getString(4)};
+            mTableModel.addRow(rows);
+        }
+    }
+    }
+
