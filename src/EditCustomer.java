3b3197a68dbd15570ba4fe00f5b15664eef71bc5
Lars <s1147485@student.windesheim.nl>
2020-05-08 11:14:26 +0200

    Database gegevens

--- src/EditCustomer.java
+++ src/EditCustomer.java
@@ -1,18 +1,23 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 class EditCustomer
 {
     // Define database variables
     private static Connection con = null;
-    private static final String URL = "jdbc:mysql://localhost:3306/wideworldimporters";
+    private static final String URL = "jdbc:mysql://localhost:3306/onzedbwwi";
     private static final String driver = "com.mysql.jdbc.Driver";
     private static final String user = "root";
     private static final String pass = "";
@@ -28,12 +33,14 @@ class EditCustomer
 
         Statement stmt;
         String query;
+        String query2;
         ResultSet rs;
+        ResultSet result;
 
         // Temporary data
-        Object[][] rowData = {{"Row1-Column1", "Row1-Column2", "Row1-Column3", "Row1-Column4"}};
+        Object[][] rowData = {{"Row1-Column1", "Row1-Column2", "Row1-Column3", "Row1-Column4", "Row1-Column5", "Row1-Column6"}};
         // Array for columnNames
-        Object[] columnNames = {"Klant naam", " Adres", "Postcode", "Stad", ""};
+        Object[] columnNames = {"Stad", "Postcode", "Straatnaam", "Huisnummer", "Voornaam", "Achternaam"};
 
         // Creating table
         DefaultTableModel mTableModel = new DefaultTableModel(rowData, columnNames);
@@ -48,7 +55,7 @@ class EditCustomer
         }
 
         // Query from customers and cities
-        query = "SELECT CustomerName, DeliveryAddressLine2, DeliveryPostalCode, CityName FROM Customers join cities on DeliveryCityID = CityID";
+        query = "SELECT City, Zip_Code, Street_Name, House_Number, First_Name, Last_Name FROM address join customer on Address_ID = Address_1";
         stmt = con.createStatement();
         // Execute query and return results
         rs = stmt.executeQuery(query);
@@ -68,9 +75,9 @@ class EditCustomer
         // For each row
         while (rs.next()) {
             // adding values to temporary rows
-            rows = new Object[]{rs.getString(1), rs.getString(2), rs.getString(3),rs.getString(4)};
+            rows = new Object[]{rs.getString(1), rs.getString(2), rs.getString(3),rs.getString(4),rs.getString(5), rs.getString(6)};
             mTableModel.addRow(rows);
         }
     }
-    }
+}
 
