27f95449b7d405d9be38c1e07bcf4aba95f3f15b
Geertjan64 <geertjanverkuil@gmail.com>
2020-05-12 20:01:18 +0200

    update

--- src/HereApp.java
+++ src/HereApp.java
@@ -1,6 +1,7 @@
 import org.json.*;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -13,13 +14,44 @@ import java.net.URL;
 
 
 public class HereApp extends JFrame implements ActionListener {
-
+    private JTextField straatnaamJtf, huisnummerJtf, stadJtf;
+    private JButton toevoegenAdres;
+    private String url;
 
     public HereApp(){
         super("Inplannen route");
-        setSize(1000,1000);
+        setSize(1200,800);
+        setLayout(new FlowLayout());
 
-        
+        JLabel straatnaamLbl = new JLabel("Straatnaam");
+        JLabel huisnummerLbl = new JLabel("Huisnummer");
+        JLabel stadLbl = new JLabel("Stad");
+
+        straatnaamJtf = new JTextField(30);
+        huisnummerJtf = new JTextField(8);
+        stadJtf = new JTextField(10);
+
+        toevoegenAdres = new JButton("Toevoegen adres");
+        toevoegenAdres.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String straatnaamStr = straatnaamJtf.getText();
+                String huisnummerStr = huisnummerJtf.getText();
+                String stadStr = stadJtf.getText();
+                url = "https://geocode.search.hereapi.com/v1/geocode?q=" + straatnaamStr + "+" + huisnummerStr + "%2C+" + stadStr + "&apiKey="+apiKey;
+                System.out.println(url);
+            }
+        });
+
+        add(straatnaamLbl);
+        add(straatnaamJtf);
+        add(huisnummerLbl);
+        add(huisnummerJtf);
+        add(stadLbl);
+        add(stadJtf);
+        add(toevoegenAdres);
+
+
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
@@ -30,7 +62,7 @@ public class HereApp extends JFrame implements ActionListener {
 
     public static void main(String[] args) throws IOException, JSONException {
 
-        URL url = new URL("https://geocode.search.hereapi.com/v1/geocode?q=Invalidenstr+117%2C+Berlin&apiKey="+apiKey);
+        URL url = new URL("https://geocode.search.hereapi.com/v1/geocode?q=vanOldenbarneveltstraat+57%2C+Nijkerk&apiKey="+apiKey);
         HttpURLConnection hr = (HttpURLConnection) url.openConnection();
 
         if(hr.getResponseCode() == 200) {
