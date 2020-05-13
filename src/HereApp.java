4ce1b8d0ec1b824454d953c8682ec30f532e6495
dylandoldersum <dylandoldersum@outlook.com>
2020-05-13 20:03:14 +0200

    added Hereapp constructor
    added main->constructor

--- src/HereApp.java
+++ src/HereApp.java
@@ -18,8 +18,29 @@ public class HereApp extends JFrame implements ActionListener {
     private JButton toevoegenAdres;
     private String url;
 
-    public HereApp(){
+    public HereApp() throws IOException, JSONException {
         super("Inplannen route");
+
+        URL apiurl = new URL("https://geocode.search.hereapi.com/v1/geocode?q=vanOldenbarneveltstraat+57%2C+Nijkerk&apiKey="+apiKey);
+        HttpURLConnection hr = (HttpURLConnection) apiurl.openConnection();
+
+        if(hr.getResponseCode() == 200) {
+            InputStream im = hr.getInputStream();
+            StringBuffer sb = new StringBuffer();
+            BufferedReader br = new BufferedReader(new InputStreamReader(im));
+            String line = br.readLine();
+
+            JSONObject response = new JSONObject(line);
+            System.out.println(response);
+
+            JSONArray array = response.getJSONArray("items");
+
+            JSONObject arrayItems = array.getJSONObject(0);
+
+            JSONObject position = arrayItems.getJSONObject("position");
+            System.out.println(position.getDouble("lat") + " : " + position.getDouble("lng"));
+        }
+
         setSize(1200,800);
         setLayout(new FlowLayout());
 
@@ -55,33 +76,12 @@ public class HereApp extends JFrame implements ActionListener {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
         setVisible(true);
+
     }
     /** HERE API INFORMATION **/
 
     public static void main(String[] args) throws IOException, JSONException {
-
-        URL url = new URL("https://geocode.search.hereapi.com/v1/geocode?q=vanOldenbarneveltstraat+57%2C+Nijkerk&apiKey="+apiKey);
-        HttpURLConnection hr = (HttpURLConnection) url.openConnection();
-
-        if(hr.getResponseCode() == 200) {
-           InputStream im = hr.getInputStream();
-           StringBuffer sb = new StringBuffer();
-           BufferedReader br = new BufferedReader(new InputStreamReader(im));
-           String line = br.readLine();
-
-           JSONObject response = new JSONObject(line);
-           System.out.println(response);
-
-           JSONArray array = response.getJSONArray("items");
-
-           JSONObject arrayItems = array.getJSONObject(0);
-
-           JSONObject position = arrayItems.getJSONObject("position");
-           System.out.println(position.getDouble("lat") + " : " + position.getDouble("lng"));
-
-           HereApp test = new HereApp();
-
-        }
+        HereApp h = new HereApp();
     }
 
     public void getAdress(){
