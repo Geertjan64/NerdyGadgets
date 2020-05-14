b097674a5988e0bc068044e702815ca521c5f0fd
Geertjan64 <geertjanverkuil@gmail.com>
2020-05-14 20:00:17 +0200

    Fix

--- src/HereApp.java
+++ src/HereApp.java
@@ -1,6 +1,9 @@
+import TSP.AlgoritmeNB;
 import TSP.BezorgerSteden;
 import TSP.Stad;
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,13 +14,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class HereApp extends JFrame implements ActionListener {
     private static String apiKey = "MLr6vmcH7IgZsaAqaSebZ42kxfRuY1SJyGdJL2GVhVk";
     private JTextField straatnaamJtf, huisnummerJtf, stadJtf;
     private JButton toevoegenAdres;
+    private JButton startenRoute;
     private BezorgerSteden bezorgerSteden = new BezorgerSteden();
     private String url;
     private double longitude;
@@ -26,30 +30,6 @@ public class HereApp extends JFrame implements ActionListener {
     public HereApp() throws IOException, JSONException {
         super("Inplannen route");
 
-        URL apiurl = new URL("https://geocode.search.hereapi.com/v1/geocode?q=vanOldenbarneveltstraat+57%2C+Nijkerk&apiKey="+apiKey);
-        HttpURLConnection hr = (HttpURLConnection) apiurl.openConnection();
-
-        if(hr.getResponseCode() == 200) {
-            InputStream im = hr.getInputStream();
-            StringBuffer sb = new StringBuffer();
-            BufferedReader br = new BufferedReader(new InputStreamReader(im));
-            String line = br.readLine();
-
-            JSONObject response = new JSONObject(line);
-            System.out.println(response);
-
-            JSONArray array = response.getJSONArray("items");
-
-            JSONObject arrayItems = array.getJSONObject(0);
-
-            JSONObject position = arrayItems.getJSONObject("position");
-
-            latitude = position.getDouble("lat");
-            longitude = position.getDouble("lng");
-
-            System.out.println(position.getDouble("lat") + " : " + position.getDouble("lng"));
-        }
-
         setSize(1200,800);
         setLayout(new FlowLayout());
 
@@ -57,9 +37,20 @@ public class HereApp extends JFrame implements ActionListener {
         JLabel huisnummerLbl = new JLabel("Huisnummer");
         JLabel stadLbl = new JLabel("Stad");
 
+        startenRoute = new JButton("Start");
+
         straatnaamJtf = new JTextField(30);
         huisnummerJtf = new JTextField(8);
         stadJtf = new JTextField(10);
+
+        startenRoute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ArrayList<Stad> steden = new ArrayList<Stad>();
+                steden.addAll(bezorgerSteden.initialSteden);
+                bezorgerSteden.printKortsteRoute(new AlgoritmeNB().vindKortsteRoute(steden));
+            }
+        });
 
         toevoegenAdres = new JButton("Toevoegen adres");
         toevoegenAdres.addActionListener(new ActionListener() {
@@ -78,6 +69,7 @@ public class HereApp extends JFrame implements ActionListener {
                     ex.printStackTrace();
                 }
 
+                System.out.println(bezorgerSteden.getInitialSteden());
 //                System.out.println(url);
             }
         });
@@ -89,6 +81,7 @@ public class HereApp extends JFrame implements ActionListener {
         add(stadLbl);
         add(stadJtf);
         add(toevoegenAdres);
+        add(startenRoute);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
--- src/TSP/BezorgerSteden.java
+++ src/TSP/BezorgerSteden.java
@@ -4,15 +4,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BezorgerSteden {
-    private ArrayList<Stad> initialSteden = new ArrayList<Stad>(Arrays.asList(
-            new Stad("Nijkerk", 52.22, 5.48611),
-            new Stad("Amsterdam", 52.370216, 4.895168),
-            new Stad("Rotterdam", 51.905445, 4.466637),
-            new Stad("Den Haag", 52.078663, 4.288788)
+    public ArrayList<Stad> initialSteden = new ArrayList<Stad>(Arrays.asList(
+//            new Stad("Nijkerk", 52.22, 5.48611),
+//            new Stad("Amsterdam", 52.370216, 4.895168),
+//            new Stad("Rotterdam", 51.905445, 4.466637),
+//            new Stad("Den Haag", 52.078663, 4.288788)
     ));
 
     public void addStad(Stad stad) {
         initialSteden.add(stad);
+    }
+
+    public String getInitialSteden() {
+        String returnValue = "";
+        for (Stad f : initialSteden) {
+            returnValue += f.getNaam() + "\n";
+        };
+        return returnValue;
     }
 
     public void printKortsteRoute(Route kortsteRoute) {
