b47438e15408ac1937dfe088ac8811f6282b14eb
PascalBoertien <57621478+PascalBoertien@users.noreply.github.com>
2020-05-21 11:33:50 +0200

    inzien route

--- src/Default/BeheerRoute.java
+++ src/Default/BeheerRoute.java
@@ -3,32 +3,57 @@ package Default;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class BeheerRoute extends JFrame{
 
     public BeheerRoute() throws SQLException {
-        setTitle("Bezorgers beheren");
+        setTitle("Beheren route");
         setSize(1200, 800);
         setResizable(false);
         setMinimumSize(new Dimension(1200, 800));
-//        String[] items = {"A", "B", "C", "D"};
-//        JList list = new JList(items);
-//
-//        list.addMouseListener(new MouseAdapter() {
-//            public void mouseClicked(MouseEvent evt) {
-//                JList list = (JList)evt.getSource();
-//                if (evt.getClickCount() == 2) {
-//
-//                    // Double-click detected
-//                    int index = list.locationToIndex(evt.getPoint());
-//                }
-//            }
-//        });
+
+        DefaultListModel<String> model = new DefaultListModel<>();
+        JList<String> list = new JList<>( model );
+
+        DatabaseReader acc = new DatabaseReader();
+        Connection dbc = acc.getConnection();
+        String query = "SELECT * FROM `optimal_route` WHERE `Delivered` = 0";
+
+        Statement st = dbc.createStatement();
+        ResultSet r = st.executeQuery(query);
+
+        while(r.next()) {
+            String routeid = r.getString("Route_ID");
+            model.addElement(routeid);
+        }
+
+        add(list);
+
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLayout(new FlowLayout());
         setLocationRelativeTo(null);
         setVisible(true);
+
+        list.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent evt) {
+                JList list = (JList)evt.getSource();
+                if (evt.getClickCount() == 2) {
+
+                    // Double-click detected
+                    int index = list.locationToIndex(evt.getPoint());
+                    int id = index+1;
+                    try {
+                        ZienRoute z = new ZienRoute(id);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
     public static void main(String[] args) throws SQLException {
---
+++ src/Default/ZienRoute.java
@@ -0,0 +1,39 @@
+package Default;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ZienRoute extends JFrame {
+
+    public ZienRoute(int id) throws SQLException {
+        setTitle("Beheren route");
+        setSize(1200, 800);
+        setResizable(false);
+        setMinimumSize(new Dimension(1200, 800));
+
+        DefaultListModel<String> model = new DefaultListModel<>();
+        JList<String> list = new JList<>(model);
+
+        DatabaseReader acc = new DatabaseReader();
+        Connection dbc = acc.getConnection();
+        String query = "SELECT * FROM `optimal_route` WHERE `Route_ID` = " + id + "";
+        Statement st = dbc.createStatement();
+        ResultSet r = st.executeQuery(query);
+        r.first();
+        String R = r.getString("Route");
+        for (String ro : R.split("\\[", 0)) {
+            model.addElement(ro);
+        }
+
+        add(list);
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new FlowLayout());
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
